900e4c210204f7e30b26f04740070b75c9a8023f
torrentul <53786970+torrentul@users.noreply.github.com>
2020-08-08 00:47:22 +0300

    Add files via upload

---
+++ src/com/company/BankAccount.java
@@ -0,0 +1,40 @@
+package com.company;
+
+import java.math.BigDecimal;
+
+public class BankAccount {
+    private BigDecimal balance;
+    private String owner;
+
+    public BankAccount(String owner) {
+        this(BigDecimal.ZERO, owner);
+    }
+
+    public BankAccount(BigDecimal balance, String owner) {
+        this.balance = balance;
+        this.owner = owner;
+    }
+
+    public synchronized void deposit(BigDecimal money) {
+        BigDecimal newBalance = balance.add(money);
+        System.out.println("Add " + money + ", current balance " + newBalance);
+        balance = newBalance;
+    }
+
+    public synchronized void withdraw(BigDecimal money) throws NotEnoughMoneyException {
+        BigDecimal newBalance = balance.subtract(money);
+
+        if (newBalance.compareTo(BigDecimal.ZERO) < 0) throw new NotEnoughMoneyException();
+
+        balance = newBalance;
+        System.out.println("Spend " + money + ", current balance " + balance);
+    }
+
+    public void deposit(String money) {
+        deposit(new BigDecimal(money));
+    }
+
+    public  void withdraw(String money) throws NotEnoughMoneyException {
+        withdraw(new BigDecimal(money));
+    }
+}
